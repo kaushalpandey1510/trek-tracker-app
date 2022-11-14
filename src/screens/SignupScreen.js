9b7fceaf39224acfafda255cb2247d0c2fe2f166
Kaushal Pandey <kaushalpandey1510@gmail.com>
2022-11-13 21:34:18 -0500

    Signup form designed and wired state mgmnt

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,14 +1,54 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Text, Input, Button } from '@rneui/themed';
+import Spacer from '../components/Spacer';
 
-const SignupScreen = () => {
+const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <View>
-      <Text>SignupScreen</Text>
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3> Sign Up for Tracker</Text>
+      </Spacer>
+      <Spacer>
+        <Input
+          label='Email'
+          value={email}
+          onChangeText={setEmail}
+          autoCapitalize='none'
+          autoCorrect={false}
+        />
+      </Spacer>
+      <Spacer>
+        <Input
+          secureTextEntry
+          label='Password'
+          value={password}
+          onChangeText={setPassword}
+          autoCapitalize='none'
+          autoCorrect={false}
+        />
+      </Spacer>
+      <Spacer>
+        <Button title='Sign Up' />
+      </Spacer>
     </View>
-  )
-}
+  );
+};
 
-export default SignupScreen
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: null,
+  };
+};
+export default SignupScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 225,
+  },
+});
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,10 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import React from 'react';
 
-const TrackListScreen = () => {
+const TrackListScreen = ({ navigation }) => {
   return (
     <View>
       <Text>TrackListScreen</Text>
+      <Button
+        title='Go to Track Detail'
+        onPress={() => navigation.navigate('TrackDetail')}
+      />
     </View>
   );
 };
