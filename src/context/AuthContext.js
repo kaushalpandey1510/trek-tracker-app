9be6159fac0f1e77828eab734fe87693cc296ea3
Kaushal Pandey <kaushalpandey1510@gmail.com>
2022-11-17 18:27:16 -0500

    navigation setup complete after signup

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,25 +1,32 @@
 import createDataContext from './createDataContext';
 import trackerApi from './../api/trackers';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { navigate } from '../navigationRef';
 
 const authReducer = (state, action) => {
   switch (action.type) {
     case 'add_error':
       return { ...state, errorMessage: action.payload };
+    case 'signup':
+      return { errorMessage: '', token: action.payload };
     default:
       return state;
   }
 };
 
-const signup = (dispatch) => {
-  return async ({ email, password }) => {
+const signup =
+  (dispatch) =>
+  async ({ email, password }) => {
     try {
       const response = await trackerApi.post('/signup', { email, password });
-      console.log(response.data);
+      await AsyncStorage.setItem('token', response.data.token);
+      dispatch({ type: 'signup', payload: response.data.token });
+
+      navigate('TrackList');
     } catch (err) {
       dispatch({ type: 'add_error', payload: 'Email already exist!' });
     }
   };
-};
 
 const signin = (dispatch) => {
   return ({ email, password }) => {};
@@ -32,5 +39,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signup, signin, signout },
-  { isSignedIn: false, errorMessage: '' }
+  { token: null, errorMessage: '' }
 );
