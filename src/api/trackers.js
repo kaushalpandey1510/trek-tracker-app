0b36c5d615d2ad5ab41175ca2d69e747eb6be84c
Kaushal Pandey <kaushalpandey1510@gmail.com>
2022-12-31 20:09:17 -0500

    circle added for user's location

--- src/api/trackers.js
+++ src/api/trackers.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://9c68-2607-fea8-1f9d-f780-1df4-24c8-44e7-c50b.ngrok.io/',
+  baseURL: 'https://5a9c-2607-fea8-1f9d-f780-fdcb-f629-2f34-3fd3.ngrok.io ',
 });
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { StyleSheet, View, ActivityIndicator } from 'react-native';
-import MapView, { Polyline } from 'react-native-maps';
+import MapView, { Circle } from 'react-native-maps';
 import { Context as LocationContext } from '../context/LocationContext';
 
 const Map = () => {
@@ -12,24 +12,42 @@ const Map = () => {
     return <ActivityIndicator size='large' style={{ marginTop: 200 }} />;
   }
 
+  initialLocation = {
+    longitude: -80.52332002677072,
+    latitude: 43.41498076919784,
+  };
+
+  // let points = [];
+  // for (let i = 0; i < 20; i++) {
+  //   if (i % 2 === 0) {
+  //     points.push({
+  //       latitude: 37.33233 + i * 0.001,
+  //       longitude: -80.52332002677072 + i * 0.001,
+  //     });
+  //   } else {
+  //     points.push({
+  //       latitude: 37.33233 - i * 0.002,
+  //       longitude: -122.03121 + i * 0.001,
+  //     });
+  //   }
+  // }
+
   return (
-    <View>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          ...currentLocation.coords,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01,
-        }}
-        region={{
-          ...currentLocation.coords,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01,
-        }}
-      >
-        {/* <Polyline coordinates={points} /> */}
-      </MapView>
-    </View>
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        ...initialLocation,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+    >
+      <Circle
+        center={currentLocation.coords}
+        radius={30}
+        strokeColor='rgba(158, 158, 255, 1.0)'
+        fillColor='rgba(158, 158, 255, 0.3)'
+      />
+    </MapView>
   );
 };
 
