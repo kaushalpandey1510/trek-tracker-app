0ff32e037828d3ac450e553c57296d2de5528c15
Kaushal Pandey <kaushalpandey1510@gmail.com>
2023-01-13 17:36:32 -0500

    disable location when screen not focused

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,15 +1,16 @@
 import '../_mockLocation';
 import React, { useContext } from 'react';
 import { StyleSheet, SafeAreaView } from 'react-native';
+import { withNavigationFocus } from 'react-navigation';
 import { Text } from '@rneui/themed';
 import Map from '../components/Map';
 import { Context as LocationContext } from '../context/LocationContext';
 import useLocation from '../hooks/useLocation';
 
-const TrackCreateScreen = () => {
+const TrackCreateScreen = ({ isFocused }) => {
   const { addLocation } = useContext(LocationContext);
-  const [err] = useLocation(addLocation);
-  
+  const [err] = useLocation(isFocused, addLocation);
+
   return (
     <SafeAreaView>
       <Text h2>Create a Track</Text>
@@ -19,6 +20,6 @@ const TrackCreateScreen = () => {
   );
 };
 
-export default TrackCreateScreen;
+export default withNavigationFocus(TrackCreateScreen);
 
 const styles = StyleSheet.create({});
